232424b0666dd363229240e5c80cc62cc9ee4fa4
sharandadi <sharanssd11@gmail.com>
2023-08-01 00:14:13 +0530

    Navbar update 1.0.9.54..5.4.3.21.1.2.34.
    hi this is me

--- src/components/navigationBar/Navbar.js
+++ src/components/navigationBar/Navbar.js
@@ -1,29 +1,36 @@
-import React from 'react'
-import {Routes, Route, Link} from 'react-router-dom';
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import HomePage from '../../pages/homePage';
-
-
-
+import social from '../../pages/social.png'; // Import the image
+import '../../pages/navbar.css';
 
 const Navbar = () => {
   return (
     <div>
-      <nav className='flex fixed top-0 h-24 w-screen m-0 bg-black text-white shadow-lg p-4 space-x-4 overflow-x-auto'>
-      <ul className='list-none flex items-center space-x-6 text-lg ml-auto'>
-          <li><Link to="/" className="text-xl">Home</Link></li>
-          <li><Link to="/services" className="text-xl">Services</Link></li>
-          <li><Link to="/timetable" className="text-xl">Portfolio</Link></li>
-          <li><Link to="/blogs" className="text-xl">Contact Us</Link></li>
-        </ul>
+      <nav className='flex fixed top-0 h-24 w-screen m-0 bg-black text-white shadow-lg p-4 space-x-4 overflow-x-auto justify-center'>
+        <div style={{ position: 'relative' }}>
+        <img src={social} alt="Logo" style={{ width: '40px', height: '40px', marginRight: '10px', position: 'absolute', top: '10px', left: '10px' }} />
+          <ul className='list-none flex items-center space-x-6 text-lg'>
+            <li><Link to="/" className="text-xl font-bold nav-link">Home</Link></li>
+            <li><Link to="/services" className="text-xl font-bold nav-link">Services</Link></li>
+            <li><Link to="/timetable" className="text-xl font-bold nav-link">Portfolio</Link></li>
+            <li><Link to="/blogs" className="text-xl font-bold nav-link">Contact Us</Link></li>
+          </ul>
+        </div>
       </nav>
+      <div className="flex justify-center mt-8">
+        <div style={{ backgroundColor: 'darkgrey', padding: '24px' }}>
+          <p style={{ textTransform: 'uppercase', fontSize: '60px', color: 'white', margin: '0' }}>
+            Take your business to the next level
+          </p>
+        </div>
+      </div>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path='/services' element={<servicesPage />} />
-        <Route path='/portfolio' element={<portfolioPage />} />
-        <Route path='/contactus' element={<contactPage />} />
+        {/* other route elements */}
       </Routes>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div className='bg-green-300 h-screen flex  w-screen mt-24 p-2'>
+    <div className='bg-black h-screen flex  w-screen mt-24 p-2'>
       <h1>Welcome!</h1>
     </div>
   )

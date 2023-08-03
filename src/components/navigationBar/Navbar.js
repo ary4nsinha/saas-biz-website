d32b649d33eb0aadb889332da034bf8eaebdd51f
sharandadi <sharanssd11@gmail.com>
2023-08-04 00:10:05 +0530

    8/4/2023
    00:09

--- src/components/navigationBar/Navbar.js
+++ src/components/navigationBar/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import HomePage from '../../pages/homePage';
 import '../../pages/navbar.css';
-import '../../pages/style.css'; // Import your CSS file for styling
+
 
 
 

aeae925afcae7861116ab21816029b18ae4f79c8
sharandadi <sharanssd11@gmail.com>
2023-08-06 00:57:11 +0530

    00P:56
    changes layout of the website.
    
    8/6/2023

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../social.png';
 import '../header/index.css';
 
 const Header = () => {
   return (
     <>
+    
+    
       <header>
+      
         <Link to="#">
-          <img src="social.png" alt="SOCIAL LOGO" />
         </Link>
         <nav>
           <ul>
--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
-import './index.css'
-
+import React from 'react';
+import './index.css';
 
 const Home = () => {
   return (
     <div className='home-comp'>
         <div className='left-col'>
-            <h1>Create beautiful and responsive websites </h1>
-            <h2>To elevate your Business to the next level!</h2>
+            <h1>We help brands like you dominate their competitors with our high quality designs </h1>
         </div>
-    </div>
-  )
+        <div className='text-box'>
+             
+            <h1>UI/UX Design</h1>
+             <p>We help you create user-friendly and responsive interfaces.</p>
+            </div>
+            <div className='button-like-text'>
+                <span className='button-background'>
+                    This text has a button-like effect.
+                </span>
+            </div>
+
+        
+        </div>
+    
+  );
 }
 
-export default Home
+export default Home;

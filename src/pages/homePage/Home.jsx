6f569a05059d4f38380368ec9033aa8eaf3f72f3
ary4nsinha <work.aryansinha@gmail.com>
2023-08-06 01:30:51 +0530

    Background image added to home screen
    Added bg image

--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -1,27 +1,24 @@
 import React from 'react';
 import './index.css';
+import DubaiSVG from './Dubai-color.svg';
 
 const Home = () => {
   return (
+    <>
     <div className='home-comp'>
-        <div className='left-col'>
-            <h1>We help brands like you dominate their competitors with our high quality designs </h1>
-        </div>
-        <div className='text-box'>
-             
-            <h1>UI/UX Design</h1>
-             <p>We help you create user-friendly and responsive interfaces.</p>
-            </div>
-            <div className='button-like-text'>
-                <span className='button-background'>
-                    This text has a button-like effect.
-                </span>
-            </div>
-
-        
-        </div>
-    
+      <div className='left-col'>
+        <h1>We help brands like you dominate their competitors with our high quality designs</h1>
+      </div>
+      <div className='center-text'>
+        <h1>Connect your business to the internet and Elevate it!</h1>
+      </div>
+      <button type="button" class="btn btn-primary">Primary</button>
+    </div>
+     <div className='dubai'>
+     <img src={DubaiSVG} alt='Dubai' />
+   </div>
+   </>
   );
-}
+};
 
 export default Home;

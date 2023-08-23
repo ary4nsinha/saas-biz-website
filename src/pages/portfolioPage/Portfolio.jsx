ce3a04fbd5766449c596fad479801c74cffa9e44
ary4nsinha <work.aryansinha@gmail.com>
2023-08-24 00:28:31 +0530

    update
    yo

--- src/pages/portfolioPage/Portfolio.jsx
+++ src/pages/portfolioPage/Portfolio.jsx
@@ -1,30 +1,34 @@
 import React from "react";
 import Card from "../../components/cards/Card";
-import image1 from '../portfolioPage/CARD-1.png';
-import image2 from '../portfolioPage/CARD-2.jpg';
-import '../portfolioPage/index.css';
+import image1 from "../portfolioPage/CARD-1.png";
+import image2 from "../portfolioPage/CARD-2.jpg";
+import "../portfolioPage/index.css";
 
 const portfolio = () => {
   return (
-    <div className="portfolio-page">
+    <>
       <div className="top-shota">
         <h1>PORTFOLIO</h1>
       </div>
-      <div className="cards">
-      <Card
-        title="App Home Page Design"
-        description="UI Design for an IOS Application home page"
-        imgSrc={image1}
-        buttonText="Learn More"
-      />
-      <Card
-        title="Home Page UI web design"
-        description="Landing page for a website"
-        imgSrc={image2}
-        buttonText="Learn More"
-      />
+      <div className="portfolio-page">
+        <div className="card-1">
+          <Card
+            title="App Home Page Design"
+            description="Sleek and intuitive, the mobile app's design offers effortless navigation with vibrant, modern aesthetics that engage users at every touchpoint."
+            imgSrc={image1}
+            buttonText="Learn More"
+          />
+        </div>
+        <div className="card-2">
+          <Card
+            title="Home Page UI web design"
+            description="Landing page for a website"
+            imgSrc={image2}
+            buttonText="Learn More"
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 

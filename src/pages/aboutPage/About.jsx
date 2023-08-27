72eade63877a39d5d0b015a9ef1ab831b53ea622
sharandadi <sharanssd11@gmail.com>
2023-08-27 12:39:58 +0530

    added elements
    added a few changes to the "aboutus" page

--- src/pages/aboutPage/About.jsx
+++ src/pages/aboutPage/About.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 
 const about = () => {
   return (
-    <div>
-      aburinevoi
+<div className="section-container">
+      <div className="title">
+        <h2>About us</h2>
+      </div>
+      <div className="content">
+        <p>Welcome to Soc1al, where creativity meets technology to bring your visions to life. As a dynamic and innovative company, we specialize in crafting unparalleled user experiences through captivating UI/UX design that seamlessly blends form and function. Our expertise extends to cutting-edge web development, where we transform intricate code into visually stunning and highly responsive websites that leave a lasting impact. Complementing our design and development prowess, we also excel at curating concise yet compelling short-form content that captures attention in today's fast-paced digital landscape. At Soc1al, we are dedicated to elevating your online presence and ensuring that every interaction, click, and scroll is an unforgettable journey. Join us in shaping digital narratives and engaging experiences that resonate.</p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default about
